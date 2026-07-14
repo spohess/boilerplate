07e13a83ac76b90864c84eed2516366aeef0dfdf
Sergio Hess <hess.sergio@gmail.com>
2026-07-13 22:46:46 -0300

    Atualização

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         __DIR__ . '/../app/Infrastructure/Console/Commands',
     ])
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('orders')
-            ->name('orders.')
-            ->group(__DIR__ . '/api/orders.php');
+    ->name('orders.')
+    ->group(__DIR__ . '/api/orders.php');
